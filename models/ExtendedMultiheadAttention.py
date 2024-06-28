602c0758977008b7600dcf025a68412b11ea3033
farjad <farjad007@live.com>
2024-06-28 19:30:08 +0200

    initial implementation again

--- models/ExtendedMultiheadAttention.py
+++ models/ExtendedMultiheadAttention.py
@@ -21,29 +21,3 @@ class ExtendedMultiheadAttention(nn.Module):
         output = torch.matmul(attn_output, self.learnable_matrix)
         
         return output, attn_output_weights
-
-
-
-# import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
-
-# class ExtendedMultiheadAttention(nn.Module):
-#     def __init__(self, embed_dim, num_heads, **kwargs):
-#         super(ExtendedMultiheadAttention, self).__init__()
-#         self.d_model = embed_dim
-#         self.nhead = num_heads
-
-#         self.mha = nn.MultiheadAttention(embed_dim, num_heads, **kwargs)
-        
-#         # Additional learnable parameter: a linear transformation
-#         self.linear = nn.Linear(embed_dim, embed_dim)
-
-#     def forward(self, query, key, value, **kwargs):
-#         # Apply multi-head attention
-#         attn_output, attn_output_weights = self.mha(query, key, value, **kwargs)
-        
-#         # Apply additional linear transformation
-#         output = self.linear(attn_output)
-        
-#         return output, attn_output_weights
